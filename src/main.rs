8a4ddab8f5e654f49276a4ecd50b7aa396f3e70c
Samson Yeung <samson_gh@onepatchdown.net>
2023-09-29 03:39:48 -0700

    Step 2: Parse HTML to Extract Comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-// Add dependencies to Cargo.toml
-[dependencies]
-reqwest = "0.11"
-tokio = { version = "1", features = ["full"] }
+extern crate reqwest;
+extern crate scraper;
 
-// main.rs
+use scraper::{Html, Selector};
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let url = "https://news.ycombinator.com/threads?id=fragmede";
     let resp = reqwest::get(url).await?;
     let body = resp.text().await?;
-    println!("body = {:?}", body);
+
+    let fragment = Html::parse_document(&body);
+    let comment_selector = Selector::parse(".commtext").unwrap();
+
+    for comment in fragment.select(&comment_selector) {
+        let comment_text = comment.text().collect::<String>();
+        println!("Comment: {}", comment_text);
+    }
+
     Ok(())
 }
