abebb4f9a76fdb453ee471cc94eacaa046741468
Samson Yeung <samson_gh@onepatchdown.net>
2023-09-29 03:41:02 -0700

    Step 4: Check for New Replies and Send Notifications

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate reqwest;
 extern crate scraper;
 extern crate rusqlite;
+extern crate notify_rust;
 
 use scraper::{Html, Selector};
 use rusqlite::{params, Connection, Result};
+use notify_rust::Notification;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,11 +28,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for comment in fragment.select(&comment_selector) {
         let comment_text = comment.text().collect::<String>();
-        conn.execute(
-            "INSERT INTO comments (text) VALUES (?1)",
-            params![comment_text],
-        )?;
-        println!("Saved comment: {}", comment_text);
+        let mut stmt = conn.prepare("SELECT id FROM comments WHERE text = ?1")?;
+        let comment_exists: Result<i32> = stmt.query_row(params![comment_text], |row| row.get(0));
+
+        if comment_exists.is_err() {
+            conn.execute(
+                "INSERT INTO comments (text) VALUES (?1)",
+                params![comment_text],
+            )?;
+            println!("New comment: {}", comment_text);
+
+            let first_10_words: String = comment_text.split_whitespace().take(10).collect::<Vec<&str>>().join(" ");
+            Notification::new()
+                .summary("New Reply on Hacker News")
+                .body(&first_10_words)
+                .show()?;
+        }
     }
 
     Ok(())
